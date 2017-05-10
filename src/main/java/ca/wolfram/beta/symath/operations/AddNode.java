90c3834ccb020815b929414c434224791c74def0
Allan Wang <me@allanwang.ca>
2017-05-09 20:59:35 -0700

    Implement AddNode flattening

--- src/main/java/ca/wolfram/beta/symath/operations/AddNode.java
+++ src/main/java/ca/wolfram/beta/symath/operations/AddNode.java
@@ -8,8 +8,8 @@ import ca.wolfram.beta.symath.base.BaseNode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * Node that will sum all of its children
@@ -37,23 +37,38 @@ public class AddNode extends OperationNode {
 
     /**
      * Extracts all constant integers and appends it as one {@link ca.wolfram.beta.symath.base.ConstantNode} if the sum != 0
+     * Flattens children if they are also of type ADD
      *
      * @return true is isConstant, false otherwise
      */
     @Override
     public boolean simplify() {
-        long constant = 0;
-        final Iterator<MathNode> each = getChildren().iterator();
+        long constant = simplify(0, 0);
+        if (constant != 0)
+            children.add(BaseNode.create(constant));
+        return super.simplify();
+    }
+
+    private int simplify(int index, int constant) {
+        List<MathNode> childrenToRaise = new ArrayList<>(); //children to be flattened to this node level
+        final ListIterator<MathNode> each = getChildren().listIterator(index); //start iteration and last checked index
         while (each.hasNext()) {
             MathNode next = each.next();
+            if (next.getType() == NodeType.ADD) {
+                childrenToRaise.addAll(next.getChildren());
+                each.remove();
+                continue;
+            }
             if (!next.isConstant() || !MathUtils.isConstantInt(next)) continue;
             each.remove();
             constant += next.eval(null);
         }
-        MathNode newConstant = BaseNode.create(constant);
-        if (newConstant.eval(null) != 0)
-            getChildren().add(newConstant);
-        return super.simplify();
+        if (!childrenToRaise.isEmpty()) {
+            int last = children.size();
+            children.addAll(childrenToRaise);
+            return simplify(last, constant); //recurse and return total constant sum
+        }
+        return constant; //done simplification; return sum
     }
 
     @Override
