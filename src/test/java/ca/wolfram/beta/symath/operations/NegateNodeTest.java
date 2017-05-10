1357d0f3f02c0098a1a56c78faf048206ed2d0bc
wollip <new Vertex(vertices.get(i).getLabel())>
2017-05-09 18:50:48 -0700

    Add simplification in NegateNode

--- src/test/java/ca/wolfram/beta/symath/operations/NegateNodeTest.java
+++ src/test/java/ca/wolfram/beta/symath/operations/NegateNodeTest.java
@@ -1,6 +1,7 @@
 package ca.wolfram.beta.symath.operations;
 
 import ca.wolfram.beta.symath.MathNode;
+import ca.wolfram.beta.symath.NodeType;
 import ca.wolfram.beta.symath.base.BaseNode;
 import org.junit.Test;
 
@@ -11,7 +12,7 @@ public class NegateNodeTest {
     @Test
     public void string() {
         MathNode negativeOne = NegateNode.create(BaseNode.create(1));
-        assertEquals("Negate string should have a negative sign in front", "- 1", negativeOne.toString());
+        assertEquals("Negate string should have a negative sign in front", "-1", negativeOne.toString());
     }
 
     @Test
@@ -28,5 +29,11 @@ public class NegateNodeTest {
     public void eval() {
         MathNode negativeOne = NegateNode.create(BaseNode.create(1));
         assertEquals(-1.0, negativeOne.eval(null), 0.001);
+    }
+
+    @Test
+    public void simplify() {
+        MathNode base = NegateNode.create(BaseNode.create(1));
+        assertEquals(NodeType.CONSTANT, base.getType());
     }
 }
