7c460940bdfc4121ea0aa2cc1d71eb1f981d88a5
wollip <new Vertex(vertices.get(i).getLabel())>
2017-05-10 22:55:12 -0700

    Refactor ConstantNode to use long

--- src/main/java/ca/wolfram/beta/symath/base/BaseNode.java
+++ src/main/java/ca/wolfram/beta/symath/base/BaseNode.java
@@ -10,10 +10,10 @@ import ca.wolfram.beta.symath.MathNode;
 public class BaseNode {
 
     public static MathNode
-            ZERO = create(0),
-            ONE = create(1);
+            ZERO = create(0l),
+            ONE = create(1l);
 
-    public static MathNode create(int i) {
+    public static MathNode create(long i) {
         return new ConstantNode(i);
     }
 
