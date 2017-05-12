21311844e542d754137841e04f0fdc59b9ba1076
Allan Wang <me@allanwang.ca>
2017-05-12 11:20:05 -0700

    Create more divide node tests

--- src/test/java/ca/wolfram/beta/symath/operations/DivideNodeTest.java
+++ src/test/java/ca/wolfram/beta/symath/operations/DivideNodeTest.java
@@ -1,6 +1,7 @@
 package ca.wolfram.beta.symath.operations;
 
 import ca.wolfram.beta.symath.MathNode;
+import ca.wolfram.beta.symath.NodeType;
 import ca.wolfram.beta.symath.base.BaseNode;
 import org.junit.Test;
 
@@ -19,13 +20,14 @@ public class DivideNodeTest {
 
     @Test
     public void string() {
-        assertEquals("1/2", "(1/2)", DivideNode.create(BaseNode.create(1), BaseNode.create(2)).toString());
+        assertEquals("(1/2)", DivideNode.create(BaseNode.create(1), BaseNode.create(2)).toString());
     }
 
     @Test
     public void complexString() {
         MathNode divide = DivideNode.create(BaseNode.create(1), AddNode.create(BaseNode.create("x"), BaseNode.create(2)));
-        assertEquals("(1/(x + 2))", "(1/(x + 2))", divide.toString());
+        assertEquals("(1/(x + 2))", divide.toString());
+        assertEquals("Division should be of type Power", NodeType.POWER, divide.getType());
     }
 
     @Test
@@ -33,6 +35,12 @@ public class DivideNodeTest {
         MathNode divide = DivideNode.create(BaseNode.create(6), BaseNode.create(2));
         assertEquals("6 / 2 = 3", "(3)", divide.toString());
         assertEquals("6 / 2 = 3", 3, (int) divide.eval(null));
+    }
+
+    @Test
+    public void complexDivision() {
+        MathNode divide = DivideNode.create(AddNode.create(BaseNode.create("x"), BaseNode.create(1)), AddNode.create(BaseNode.create("x"), BaseNode.create(2)));
+        assertEquals("((x + 1) / (x + 2))", divide.toString());
     }
 
     @Test
