727ee1e125f8b7823d15996a7a3f88e7744abb52
wollip <new Vertex(vertices.get(i).getLabel())>
2017-05-12 01:34:17 -0700

    Add like term related functionality to MathUtils

--- src/main/java/ca/wolfram/beta/symath/MathUtils.java
+++ src/main/java/ca/wolfram/beta/symath/MathUtils.java
@@ -1,6 +1,11 @@
 package ca.wolfram.beta.symath;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created by Allan Wang on 2017-05-09.
@@ -17,6 +22,33 @@ public class MathUtils {
 
     public static void print(String s, Object... o) {
         System.out.println(String.format(Locale.CANADA, s, o));
+    }
+
+    public static Set<MathNode> getVariableTerms(MathNode node) {
+        Set<MathNode> set;
+        switch (node.getType()) {
+        case CONSTANT:
+        case MATH_CONSTANT:
+            set = new HashSet<MathNode>();
+            break;
+        case VARIABLE:
+            set = new HashSet<MathNode>();
+            set.add(node);
+            break;
+        default:
+            set = node.getChildren().stream().filter(children -> !children.isConstant()).collect(Collectors.toSet());
+            break;
+        }
+        return set;
+    }
+
+    public static boolean isLikeTerm(MathNode n1, MathNode n2) {
+        if (n1.getType() != NodeType.MULTIPLY || n2.getType() != NodeType.MULTIPLY) return false;
+        return getVariableTerms(n1).equals(getVariableTerms(n2));
+    }
+
+    public static Map<Set<MathNode>, List<MathNode>> groupByLikeTerms(List<MathNode> nodes) {
+        return nodes.stream().collect(Collectors.groupingBy(node -> getVariableTerms(node)));
     }
 
 }
